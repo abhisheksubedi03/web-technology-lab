beabe9c2fc778d0c9bfe2ffabf77af4269ce639b
abhishek subedi <077bcsit003.abhishek@scst.edu.np>
2023-10-06 17:48:09 +0545

    js

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    const fetchButton = document.getElementById("fetchButton");
-    const usernameInput = document.getElementById("username");
-    const resultDiv = document.getElementById("result");
-  
-    fetchButton.addEventListener("click", async function() {
-      const username = usernameInput.value.trim();
-      if (username === "") {
-        resultDiv.innerHTML = "Please enter a GitHub username.";
-        return;
-      }
-  
-      try {
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        if (response.status === 404) {
-          resultDiv.innerHTML = "User not found.";
-        } else {
-          const userData = await response.json();
-          resultDiv.innerHTML = `
-            <p><h3>Name:</h3> ${userData.name}</p>
-            <p><h3>Username:</h3> ${userData.login}</p>
-            <p><h3>Location:</h3> ${userData.location || "N/A"}</p>
-            <p><h3>Followers:</h3> ${userData.followers}</p>
-            <p><h3>Following:</h3> ${userData.following}</p>
-            <p><h3>Public Repositories:</h3> ${userData.public_repos}</p>
-          `;
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-        resultDiv.innerHTML = "An error occurred while fetching data.";
-      }
-    });
-  });
-  
+
+
+
+
+function changeStatus(statusId) {
+  const statusElement = document.getElementById(`${statusId}`);
+  const statusButton = document.querySelector(`#${statusId}-btn`);
+  // const currentStatus = statusElement.textContent.trim();
+  const currentStatus = statusElement.innerText;
+  if (currentStatus === 'Published') {
+      statusElement.innerText = 'Archived';
+      statusButton.innerText = 'Draft';
+  } else if (currentStatus === 'Archived') {
+      statusElement.innerText = 'Drafted';
+      statusButton.innerText = 'Publish';
+  } else {
+      statusElement.innerText = 'Published';
+      statusButton.innerText = 'Archive';
+  }
+}
+
+function addComment(commentId) {
+  const commentsCount = parseInt(document.getElementById(commentId).textContent);
+  document.getElementById(commentId).textContent = commentsCount + 1;
+}
+
+function simulateVisit(viewsId) {
+  const viewsCount = parseInt(document.getElementById(viewsId).textContent);
+  document.getElementById(viewsId).textContent = viewsCount + 1;
+}
